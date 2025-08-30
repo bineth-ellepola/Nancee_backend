4709ef0de805b602bbd8b2e4940f9e8739903723
Bineth <binethellepola@gmail.com>
2025-08-30 12:08:46 +0530

    added mongo db newest version

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log("bye");
+//SdpL7XmOler1Bm1v
+//vCBFFqBwoUzH9sBE
+
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+//Middleware
+app.use("/", (req, res, next) => {
+  req.setEncoding("it is working");
+});
+
+mongoose.connect("mongodb+srv://admin:pGnvvM7oSKBonwNZ@cluster0.ysesjrj.mongodb.net/")
+.then(()=> console.log("Connected to mongo DB"))
+.then(() => {
+    app.listen(5000);
+})
+.catch((err)=> console.log(err));
